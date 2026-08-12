658ed99d49b68dd2457b72523c208ecb584b1bb9
Dispatch <dispatch@leelectrical.us>
2026-08-12 14:58:32 -0400

    chore(pro): stamp le-pro-v439 build

--- app/pro/assets/index-De0Q4M4i.js
+++ app/pro/assets/index-De0Q4M4i.js
@@ -1,4 +1,4 @@
-import{g as Xu}from"./index-DJZi4lHk.js";/*! *****************************************************************************
+import{g as Xu}from"./index-JFpY-knW.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
