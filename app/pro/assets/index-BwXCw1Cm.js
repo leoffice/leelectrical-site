9e9ed650b90f1a08a8e6f8b668e2067a8f23f113
Dispatch <dispatch@leelectrical.us>
2026-08-14 15:10:09 -0400

    chore(pro): stamp le-pro-v447 deploy build (f92b721)
    Post deploy-prod rebuild assets that match the live Pages ship.

--- app/pro/assets/index-BwXCw1Cm.js
+++ app/pro/assets/index-BwXCw1Cm.js
@@ -1,4 +1,4 @@
-import{g as Xu}from"./index-eg_XpmIK.js";/*! *****************************************************************************
+import{g as Xu}from"./index-Chp2uofZ.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
