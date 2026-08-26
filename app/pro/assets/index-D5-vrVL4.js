2d59b9c390f023b34e1e8a153e6a7435ec5907a8
Dispatch <dispatch@leelectrical.us>
2026-08-25 23:36:58 -0400

    chore(pro): stamp le-pro-v469 live build assets (Edit-first fix)

--- app/pro/assets/index-D5-vrVL4.js
+++ app/pro/assets/index-D5-vrVL4.js
@@ -1,4 +1,4 @@
-import{g as Xu}from"./index-DmDfBBSI.js";/*! *****************************************************************************
+import{g as Xu}from"./index-SDOH3Tbz.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
