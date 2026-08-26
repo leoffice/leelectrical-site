549e688e90613f2f52051b2b5b9135eb784073ed
Dispatch <dispatch@leelectrical.us>
2026-08-25 23:31:15 -0400

    chore(pro): bump SW to le-pro-v468 after Edit-vanish fix
    Force clients to pick up the EmailInsight Edit-first fix.

--- app/pro/assets/index-BPvf0oeb.js
+++ app/pro/assets/index-BPvf0oeb.js
@@ -1,4 +1,4 @@
-import{g as Xu}from"./index-BINkd6zd.js";/*! *****************************************************************************
+import{g as Xu}from"./index-DmDfBBSI.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
