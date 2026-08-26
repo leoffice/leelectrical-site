7772cc3b29b0be5ea493168cbd6c09ed48c80623
Dispatch <dispatch@leelectrical.us>
2026-08-26 18:08:15 -0400

    chore(pro): stamp le-pro-v471 live build assets (deposit badge + snappy send)

--- app/pro/assets/index-FBD5ongd.js
+++ app/pro/assets/index-FBD5ongd.js
@@ -1,4 +1,4 @@
-import{g as Xu}from"./index-CKVDMaqX.js";/*! *****************************************************************************
+import{g as Xu}from"./index-DpximWMV.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
