0df1b0b2aac730cf54970db41d4c78ba9abf8a1a
Dispatch <dispatch@leelectrical.us>
2026-08-28 15:31:10 -0400

    chore(pro): stamp le-pro-v474 live build assets (polish fix + save-train)

--- app/pro/assets/index-B7ygQpd_.js
+++ app/pro/assets/index-B7ygQpd_.js
@@ -1,4 +1,4 @@
-import{g as Xu}from"./index-C-dQ4V2b.js";/*! *****************************************************************************
+import{g as Xu}from"./index-D5vS5tmg.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
