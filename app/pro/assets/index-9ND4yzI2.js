d2395508f178bcacbc9ac491bca8080dea7bab20
Dispatch <dispatch@leelectrical.us>
2026-08-28 14:47:57 -0400

    chore(pro): stamp le-pro-v473 live build assets (polish train + questionnaire)

--- app/pro/assets/index-9ND4yzI2.js
+++ app/pro/assets/index-9ND4yzI2.js
@@ -1,4 +1,4 @@
-import{g as Xu}from"./index-D7ne0sCF.js";/*! *****************************************************************************
+import{g as Xu}from"./index-C-dQ4V2b.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
