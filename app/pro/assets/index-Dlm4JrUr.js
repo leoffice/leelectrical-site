0fff4adf960eb2dd60a551edafbc671e4358145e
Dispatch <dispatch@leelectrical.us>
2026-09-01 19:46:35 -0400

    chore(pro): stamp le-pro-v478 live build assets (snappy payment pack)

--- app/pro/assets/index-Dlm4JrUr.js
+++ app/pro/assets/index-Dlm4JrUr.js
@@ -1,4 +1,4 @@
-import{g as Xu}from"./index-D5vS5tmg.js";/*! *****************************************************************************
+import{g as Xu}from"./index-qnxXsttq.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
